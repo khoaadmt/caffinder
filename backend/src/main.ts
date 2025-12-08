ecf3f3be9c52f1098d26b68a0aadf5073be23cef
khoaadmt <khoaadmt@gmail.com>
2025-12-08 13:55:12 +0700

    fix(cors): allow PATCH method

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: 'Content-Type, Authorization, token',
   });
   app.setGlobalPrefix('api');
